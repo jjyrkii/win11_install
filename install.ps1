d6dfe556578d497ccbd8b051929749dc5acfd594
jyrki <jyrki1887@gmail.com>
2022-01-15 13:10:10 +0100

    initial script and winget packagelist

--- install.ps1
+++ install.ps1
@@ -1,27 +1,30 @@
 # windows 11 post-installation-script
 
 # update all pre installed packages
-#winget upgrade --all
+winget upgrade --all
 
-# install packages
-#winget import packagelist.json
+# install winget packages
+winget import packagelist.json
 
+# files not included in winget
 $downloadFiles = @{
     "https://download.amd.com/software/AMD_StoreMI.zip" = "c:\driver\StoreMI.zip";
     "https://prod.escapefromtarkov.com/launcher/download" = "c:\driver\bsg-launcher.exe";
-    "https://www.netacad.com/portal/resources/file/a1a73d9d-5af7-4162-9a52-0746d39c2dd4" = "c:\driver\packettracer.exe";
-
 }
+
+# create download directory
 echo "Creating directory to store files"
 mkdir c:\driver
 
+# download files from external sources
 echo "`v ---Starting external downloads--- `v"
 foreach ($link in $downloadFiles.Keys)
 {
     echo "Downloading: $link"
-#    Invoke-WebRequest -Uri $link -OutFile $downloadFiles[$link]
+    Invoke-WebRequest -Uri $link -OutFile $downloadFiles[$link]
 }
 
+# links to amd drivers
 echo "`vAMD-Driver:"
 echo "Chipset: https://www.amd.com/en/support/chipsets/amd-socket-am4/b450"
 echo "GPU: https://www.amd.com/en/support/graphics/radeon-500-series/radeon-rx-500-series/radeon-rx-580 `v"
